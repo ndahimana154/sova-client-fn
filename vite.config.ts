300fdbdf3948d295ce59177e5c43a0c9a1b574a4
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-07-27 23:56:32 +0200

    change env variable names

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/backend': {
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/backend/, ''),
+          target: env.VITE_BACKEND_PROXY_TARGET || 'http://localhost:7070',
+        },
+      },
+    },
+  }
 })
